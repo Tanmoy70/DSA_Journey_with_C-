fe9bff2b59d0e761c46d1aee3d4c692a9670a1b9
Tanmoy70 <tanmoykanrar1@gmail.com>
2025-10-16 00:21:09 +0530

    add Armstrong number program implementation

---
+++ armstrong-number.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int sum=0;
+    int originalNumber=n;
+    while(n>0){
+        int lastDigit=n%10;
+        sum+=pow(lastDigit,3); // sum+=lastDigit*lastDigit*lastDigit;
+        n=n/10;
+    }
+
+    if(sum==originalNumber){
+        cout<<"Armstrong Number"<<endl;
+    }
+    else{
+        cout<<"Not an Armstrong Number"<<endl;
+    }
+    return 0;
+}
