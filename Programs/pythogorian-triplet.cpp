4916bf5fd54e8a6e6245e412cb0f42135ac569a1
Tanmoy70 <tanmoykanrar1@gmail.com>
2025-10-30 00:25:29 +0530

    Add program to check for Pythagorean triplets

---
+++ Programs/pythogorian-triplet.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool check(int x, int y, int z){
+    
+}
+
+int32_t main(){
+    int x,y,z;
+    
+    cin >> x >> y >> z;
+
+    if(check(x,y,z)){
+        cout << "Pythogorian Triplete";
+    }
+    else{
+        cout << "Not a Pythogorian Triplet";
+    }
+
+}
