3a9fd416a6a3ec4debefeff8697a51a28b49cdec
Tanmoy70 <tanmoykanrar1@gmail.com>
2025-10-30 00:19:47 +0530

    Add program to calculate the sum of first N natural numbers

--- Programs/sum-firstN-natural-numbers.cpp
+++ Programs/sum-firstN-natural-numbers.cpp
@@ -1,6 +1,17 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
+int sum(int n){
+    int ans = 0;
+    for(int i = 1; i <= n; i++){
+        ans += i;
+    }
+    return ans; 
+}
+
 int32_t main(){
+    int n;
+    cin >> n;
+    cout << sum(n) << endl;
     return 0;
 }
