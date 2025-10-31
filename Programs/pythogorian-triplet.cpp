feea4c162e76c008151bbc4f7746c7647aba024e
Tanmoy70 <tanmoykanrar1@gmail.com>
2025-11-01 01:25:54 +0530

    Refactor Pythagorean triplet program for clarity and fix output messages

--- Programs/pythogorian-triplet.cpp
+++ Programs/pythogorian-triplet.cpp
@@ -2,7 +2,30 @@
 using namespace std;
 
 bool check(int x, int y, int z){
-    
+    int a = max(x, max(y,z));
+    int b, c;
+
+    if(a == x){
+        b=y;
+        c=z;
+
+    }
+    else if(a == y){
+        b=x;
+        c=z;
+    }
+    else{
+        b=x;
+        c=y;
+    }
+
+    if(a*a == b*b + c*c){
+        return true;
+    }
+    else{
+        return false;
+    }   
+
 }
 
 //This is Pythogorian triplet if a^2 + b^2 = c^2
@@ -13,10 +36,10 @@ int32_t main(){
     cin >> x >> y >> z;
 
     if(check(x,y,z)){
-        cout << "Pythogorian Triplete";
+        cout << "Pythogorian Tripletet\n";
     }
     else{
-        cout << "Not a Pythogorian Triplet";
+        cout << "Not a Pythogorian Tripletet\n";
     }
 
 }
