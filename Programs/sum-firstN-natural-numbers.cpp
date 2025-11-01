550e59315328e631578fd668b0d522084d69e9e7
Tanmoy70 <tanmoykanrar1@gmail.com>
2025-11-02 01:30:15 +0530

    Add comments to clarify the purpose of the sum function and driver code

--- Programs/sum-firstN-natural-numbers.cpp
+++ Programs/sum-firstN-natural-numbers.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Function to calculate sum of first N natural numbers
 int sum(int n){
     int ans = 0;
     for(int i = 1; i <= n; i++){
@@ -9,6 +10,7 @@ int sum(int n){
     return ans; 
 }
 
+//Driver code
 int32_t main(){
     int n;
     cin >> n;
